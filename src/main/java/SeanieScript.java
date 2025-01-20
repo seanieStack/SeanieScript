184d1aee700ff9f4e9828fa0b89f9aae740c86bf
Seanie Stack <seaniestack345@gmail.com>
2025-01-20 22:56:19 +0000

    implemented basic scanner and tokenizer

---
+++ src/main/java/SeanieScript.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+
+public class SeanieScript {
+
+    static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
+        if(args.length != 1) {
+            System.out.println("Usage: java SeanieScript <filename>");
+            System.exit(1);
+        } else {
+            runFile(args[0]);
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        if(hadError){
+            System.exit(2);
+        }
+    }
+
+    private static void run(String source){
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for(Token token : tokens){
+            System.out.println(token);
+        }
+    }
+
+    static void error(int line, String message){
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message){
+        System.err.printf("[line %d] Error %s: %s%n", line, where, message);
+        hadError = true;
+
+    }
+}
---
+++ src/main/java/Token.java
@@ -0,0 +1,13 @@
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Token {
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+    public String toString(){
+        return type + " " + lexeme + " " + literal;
+    }
+}
